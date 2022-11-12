3af514b0fd6544079f8743cee96b3ed084d62f5e
saadrada <saadrada742@gmail.com>
2022-11-12 13:16:09 +0100

    create colors and themecontext files, create switch component switch light to dark

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Switch
+        value={theme === 'light'}
+        onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+        <Text>{theme}</Text>
     </View>
   );
 }
